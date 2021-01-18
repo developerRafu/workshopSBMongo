4861c98a4e0aed489d798b8fb2eac682480edcb6
Rafael Henrique <fel.rique71@gmail.com>
2021-01-18 19:18:52 -0300

    Projeto criado

---
+++ src/main/java/com/rafu/workshop/WorkshopSbMongoApplication.java
@@ -0,0 +1,13 @@
+package com.rafu.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSbMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSbMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafu/workshop/WorkshopSbMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafu.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSbMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
