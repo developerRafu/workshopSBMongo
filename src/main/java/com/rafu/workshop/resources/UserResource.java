72b5d13e0be05a160036a1d5bf76b946e45bc6e5
Rafael Henrique <fel.rique71@gmail.com>
2021-01-19 10:04:12 -0300

    DTO

--- src/main/java/com/rafu/workshop/resources/UserResource.java
+++ src/main/java/com/rafu/workshop/resources/UserResource.java
@@ -3,6 +3,8 @@ package com.rafu.workshop.resources;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.rafu.workshop.UserService.UserService;
 import com.rafu.workshop.domain.User;
+import com.rafu.workshop.dto.UserDTO;
 
 @RestController
 @RequestMapping(value="/users")
@@ -21,9 +24,10 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User> >findAll(){
+	public ResponseEntity<List<UserDTO> >findAll(){
 		
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> dto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(dto);
 	}
 }
