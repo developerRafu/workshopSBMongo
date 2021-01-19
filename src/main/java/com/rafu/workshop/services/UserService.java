5b29ae23d9b8dd959b135994d91e1ec13798c571
Rafael Henrique <fel.rique71@gmail.com>
2021-01-19 10:36:49 -0300

    Inserir usuario com post

--- src/main/java/com/rafu/workshop/services/UserService.java
+++ src/main/java/com/rafu/workshop/services/UserService.java
@@ -6,25 +6,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rafu.workshop.domain.User;
+import com.rafu.workshop.dto.UserDTO;
 import com.rafu.workshop.repository.UserRepository;
 import com.rafu.workshop.services.exceptions.ObjectNotFoundException;
 
 import java.util.Optional;
 
-
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
 
-	public List<User> findAll(){
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 
-}
 	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+}
