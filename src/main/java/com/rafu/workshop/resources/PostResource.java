ed00bcbb1527547d6f1be7f2d55cb1fc04d5d5c8
Rafael Henrique <fel.rique71@gmail.com>
2021-01-19 13:38:06 -0300

    Consulta com vários critérios

--- src/main/java/com/rafu/workshop/resources/PostResource.java
+++ src/main/java/com/rafu/workshop/resources/PostResource.java
@@ -1,30 +1,19 @@
 package com.rafu.workshop.resources;
 
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rafu.workshop.domain.Post;
-import com.rafu.workshop.domain.User;
-import com.rafu.workshop.dto.UserDTO;
 import com.rafu.workshop.resources.util.URL;
 import com.rafu.workshop.services.PostService;
-import com.rafu.workshop.services.UserService;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -45,4 +34,17 @@ public class PostResource {
 		List<Post> list = service.findByTitle(text);
 		return ResponseEntity.ok().body(list);
 	}
+	
+	@RequestMapping(value = "/fullsearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullSearch(@RequestParam(value="text",defaultValue = "") String text,
+											     @RequestParam(value="minDate",defaultValue = "") String minDate,
+											     @RequestParam(value="maxDate",defaultValue = "") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate,new Date(0L));
+		Date max = URL.convertDate(maxDate,new Date());
+		List<Post> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	
 }
--- src/main/java/com/rafu/workshop/services/PostService.java
+++ src/main/java/com/rafu/workshop/services/PostService.java
@@ -1,5 +1,6 @@
 package com.rafu.workshop.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rafu.workshop.domain.Post;
-import com.rafu.workshop.domain.User;
-import com.rafu.workshop.dto.UserDTO;
 import com.rafu.workshop.repository.PostRepository;
 import com.rafu.workshop.services.exceptions.ObjectNotFoundException;
 
@@ -27,5 +26,10 @@ public class PostService {
 	
 	public List<Post> findByTitle(String txt){
 		return repo.searchTile(txt);
+	}
+	
+	public List<Post> fullSearch(String txt, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24*60*60*1000);
+		return repo.fullSearch(txt, minDate, maxDate);
 	}
 }
