6c656bf159d1884aed15faa5ebbf9dbfb1e846fc
Rafael Henrique <fel.rique71@gmail.com>
2021-01-19 12:29:23 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/rafu/workshop/config/Instantiation.java
+++ src/main/java/com/rafu/workshop/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.rafu.workshop.domain.Post;
 import com.rafu.workshop.domain.User;
 import com.rafu.workshop.dto.AuthorDTO;
+import com.rafu.workshop.dto.CommentDTO;
 import com.rafu.workshop.repository.PostRepository;
 import com.rafu.workshop.repository.UserRepository;
 
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null,sdf.parse("21/03/2018"),"Partiu viagem","Vou viajara  para são paulo abraços",new AuthorDTO(maria));
 		Post post2 = new Post(null,sdf.parse("23/03/2018"),"Bom dia","Acordei feliz hoje!",new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano",sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite",sdf.parse("21/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia",sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 		
